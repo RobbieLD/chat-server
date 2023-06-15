dae849569a9f9a29e267cdffc262474c15008633
Rob Davis <robd@youi.com>
2023-06-15 14:25:32 +1000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Express and Socket.IO Chat Server
+This is a little demo chat server
+
+## Getting Started
+To run this locally, once you've fun `npm install` you can simply start the server with `npm run start`
---
+++ index.js
@@ -0,0 +1,41 @@
+const app = require('express')()
+const http = require('http').Server(app)
+const cors = require('cors')
+const io = require('socket.io')(http, { cors: { origin: "*" } })
+
+// Install the cors middleware
+app.use(cors())
+
+// We set the port this way to the environment variable PORT can override the default of 3000
+const port = process.env.PORT || 3000
+
+
+const htmlIndexHandler = (request, response) => {
+    // Just sent back the html page
+    response.sendFile(__dirname + '/index.html');
+}
+
+const chatHandler = (message) => {
+    console.log(message)
+
+    // Send the message to all connected clients
+    io.emit('chat', message)
+}
+
+const connectionHandler = (socket) => {
+    console.log(`A client has connected with id: ${socket.id}`)
+    socket.on('chat', chatHandler)
+}
+
+const serverStartHandler = () => {
+    console.log('Server is running')
+}
+
+// Hook up the event handler for the express server for the default url
+app.get('/', htmlIndexHandler);
+
+// Handle the connection event
+io.on('connection', connectionHandler);
+
+// Start the server with 
+http.listen(port, serverStartHandler);
